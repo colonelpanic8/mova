f858908ee1968a56e1bb3c4728a7dd67a7e28160
Ivan Malison <IvanMalison@gmail.com>
2026-01-19 14:27:53 -0500

    test: add expo-router mock for body editor navigation

--- tests/components/SearchScreen.test.tsx
+++ tests/components/SearchScreen.test.tsx
@@ -53,6 +53,15 @@ jest.mock("../../context/MutationContext", () => ({
     triggerRefresh: jest.fn(),
   }),
 }));
+jest.mock("expo-router", () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+    back: jest.fn(),
+    replace: jest.fn(),
+  }),
+  useLocalSearchParams: () => ({}),
+  useSegments: () => [],
+}));
 
 // Import after mocks are set up
 import { useAuth } from "../../context/AuthContext";
